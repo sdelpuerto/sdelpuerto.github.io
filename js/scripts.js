1441a32609095d40ba7e2f2266f25389aa55e9e7
Sergio del Puerto <sergiodelpuertomartin@gmail.com>
2025-05-26 14:34:35 -0700

    Remove multi-language support

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,19 @@
-(function() {
-  const $html = $("html");
-  const $flags = $(".flag");
-  const $navbar = $(".navbar-collapse");
-
+(function () {
   const form = getForm();
   const errorMessages = getErrorMessages();
 
-  function changeLanguage(lang) {
-    $html.attr("lang", lang);
-    $flags.removeClass("active");
-    $flags.filter(".flag-" + lang).addClass("active");
-
-    for (const field in form) {
-      const element = form[field].$element.get(0);
-      element.placeholder = form[field].placeholder[lang];
-    }
-  }
-
   function onChangeInvalid(e) {
-    const lang = $html.attr("lang");
     this.setCustomValidity("");
 
     if (!this.value) {
-      this.setCustomValidity(errorMessages[lang].required);
+      this.setCustomValidity(errorMessages.required);
     }
     else if (this.type === "email" && this.validity.typeMismatch) {
-      this.setCustomValidity(errorMessages[lang].email);
+      this.setCustomValidity(errorMessages.email);
     }
   }
 
-  $(document).ready(function() {
-    changeLanguage("en");
-  });
-
-  $flags.click(function(e) {
-    const lang = $(this).data("lang");
-    changeLanguage(lang);
-    $navbar.collapse("hide");
-  });
-
-  $(".modal").on("hidden.bs.modal", function() {
+  $(".modal").on("hidden.bs.modal", function () {
     var video = $(this).find("video").get(0);
     if (video) {
       video.pause();
@@ -48,53 +22,23 @@
   });
 
   for (const field in form) {
-    const $element = form[field].$element;
+    const $element = form[field];
     $element.on("change invalid", onChangeInvalid);
   }
 
   function getForm() {
     return {
-      name: {
-        $element: $("[name=name]"),
-        placeholder: {
-          en: "Name",
-          es: "Nombre"
-        }
-      },
-      email: {
-        $element: $("[name=email]"),
-        placeholder: {
-          en: "Email address",
-          es: "Correo electrónico"
-        }
-      },
-      phone: {
-        $element: $("[name=phone]"),
-        placeholder: {
-          en: "Phone number (optional)",
-          es: "Teléfono (opcional)"
-        }
-      },
-      message: {
-        $element: $("[name=message]"),
-        placeholder: {
-          en: "Message",
-          es: "Mensaje"
-        }
-      }
+      name: $("[name=name]"),
+      email: $("[name=email]"),
+      phone: $("[name=phone]"),
+      message: $("[name=message]"),
     };
   }
 
   function getErrorMessages() {
     return {
-      en: {
-        required: "This field is required.",
-        email: "Enter a valid email address."
-      },
-      es: {
-        required: "Rellene este campo.",
-        email: "Introduzca una dirección de correo."
-      }
+      required: "This field is required.",
+      email: "Enter a valid email address."
     };
   };
 })();
